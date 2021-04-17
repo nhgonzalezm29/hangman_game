10918970cfa58b1fa297904edc605ab7b19d6191
Nicolas Gonzalez <nhgonzalezm29@hotmail.com>
2021-04-17 16:49:06 -0500

    Se añaden comentarios a las diferentes funciones del programa

--- hangman.py
+++ hangman.py
@@ -4,7 +4,7 @@ import os
 #Define la constante de la cantidad de intentos
 INTENTOS = 10
 
-
+#Obtiene la palabra a adivinar desde el archivo con el listado de palabras
 def set_palabra():
     palabras = []
     with open ("./data/data.txt","r",encoding="utf-8") as f:
@@ -13,6 +13,8 @@ def set_palabra():
     return palabras[random.randrange(0,len(palabras)+1)]
 
 
+#Define el listado de diccionarios que contiene cada una de las letras de una palabra con su respectivo estado inicial
+#Recibe como parametro la palabra seleccionada del archivo
 def set_lista_inicio(palabra):
     board = []
    
@@ -22,6 +24,8 @@ def set_lista_inicio(palabra):
     return board
 
 
+#Dibuja el tablero del juego junto con el titulo y el estado de la partida.
+#Recibe como parametros el listado de las letras junto con la cantidad de intentos que quedan en la partida. 
 def draw_board(lista_letras,intentos):
     table = []
     for l in lista_letras:
@@ -41,6 +45,8 @@ def draw_board(lista_letras,intentos):
     print(table)
 
 
+#Se encarga de actualizar el listado de las letras despues de que se ingresara una letra para comparar
+#Recibe como parametro la letra ingresada por el usuario junto con el listado de las letras y su estado actual
 def update_lista(letra,lista_letras):
     new_list = []
     for l in lista_letras:
@@ -51,6 +57,8 @@ def update_lista(letra,lista_letras):
     return new_list           
 
 
+#Actualiza la cantidad de intentos que tiene el usuario.
+#Recibe como parametro la lista de letras con el estado actual, la letra ingresada por el usuario y la cantidad de intentos que actualmente se tienen.
 def update_intentos(lista_letras,letra,intentos):
     for l in lista_letras:
         for key,value in l.items():
@@ -59,6 +67,8 @@ def update_intentos(lista_letras,letra,intentos):
     return intentos-1
 
 
+#Determina el estado actual del juego hasta determinar si el usuario adivino todas las letras para declararlo ganador
+#Recibe como parametro la lista de letras con su estado actualizado
 def update_game(lista_letras):
     for l in lista_letras:
         for key,value in l.items():
